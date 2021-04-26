396d62f3aeb4f792060397f771ccd24ba45432af
Dan Knights <daniel.knights@28b.co.uk>
2021-04-26 22:21:13 +0100

    commands

---
+++ src/commands.ts
@@ -0,0 +1,44 @@
+import { commands, ShellExecution, Task, tasks, TaskScope, window } from 'vscode'
+
+function runMintCommandAsTask(subcommand: string) {
+  const definition = { command: '', type: '' }
+  const execution = new ShellExecution(`mint ${subcommand}`)
+  const task = new Task(definition, TaskScope.Workspace, '', '', execution)
+
+  tasks.executeTask(task)
+}
+
+export default class Commands {
+  constructor() {
+    commands.registerCommand('mint-pie.build', () => runMintCommandAsTask('build'))
+    commands.registerCommand('mint-pie.clean', () => runMintCommandAsTask('clean'))
+    commands.registerCommand('mint-pie.compile', () => runMintCommandAsTask('compile'))
+    commands.registerCommand('mint-pie.docs', () => runMintCommandAsTask('docs'))
+    commands.registerCommand('mint-pie.format', () => runMintCommandAsTask('format'))
+    commands.registerCommand('mint-pie.init', () => this.init())
+    commands.registerCommand('mint-pie.install', () => runMintCommandAsTask('install'))
+    commands.registerCommand('mint-pie.loc', () => runMintCommandAsTask('loc'))
+    commands.registerCommand('mint-pie.ls', () => runMintCommandAsTask('ls'))
+    commands.registerCommand('mint-pie.start', () => runMintCommandAsTask('start'))
+    commands.registerCommand('mint-pie.test', () => runMintCommandAsTask('test'))
+    commands.registerCommand('mint-pie.version', () => runMintCommandAsTask('version'))
+  }
+
+  private async init() {
+    const projectName = await window.showInputBox({
+      prompt: 'Type the name of your project',
+      placeHolder: 'mint-project',
+    })
+
+    const folder = await window.showOpenDialog({
+      canSelectFiles: false,
+      canSelectFolders: true,
+      canSelectMany: false,
+      openLabel: 'Create project',
+    })
+
+    const newProjectRoot = (folder && `${folder[0].path}/${projectName}`) || ''
+
+    runMintCommandAsTask(`init ${newProjectRoot}`)
+  }
+}
